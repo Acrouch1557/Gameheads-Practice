a7228ab1fedcd0692f5fb5b89e174e73a36f79e8
Anaya Crouch <acrouch1557@gmail.com>
2020-05-27 15:14:37 -0700

    MOve Script from old project into New

---
+++ IntroUnitySummer2/Assets/Scripts/Move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector3 speed; //Amount of units to move per second
+
+    //Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        Vector3 currentSpeed = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentSpeed.x = -speed.x;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            currentSpeed.x = speed.x;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            currentSpeed.z = speed.z;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            currentSpeed.z = -speed.z;
+        }
+
+        //Speed is a vector3 defines how fast to move in 3D space
+        gameObject.transform.Translate(currentSpeed * Time.deltaTime);
+    }
+}
